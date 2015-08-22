a836e9a58bd1ccbfa02c8bd6b3bab8b6c12eea78
Lewis Sparlin <lewis.sparlin@icloud.com>
2015-08-22 14:29:17 -0500

    Removed unused imports

--- src/main/java/com/lewismsparlin/rest/SimpleResource.java
+++ src/main/java/com/lewismsparlin/rest/SimpleResource.java
@@ -4,9 +4,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
-import com.google.gson.Gson;
 import com.lewismsparlin.model.SimpleObject;
 
 @Path("simple")
@@ -16,7 +14,7 @@ public class SimpleResource {
   @Produces(MediaType.APPLICATION_JSON)
   public SimpleObject sayHello() {
     SimpleObject object = new SimpleObject("A Name", "A Description");
-		return object;
+    return object;
   }
 
 }
